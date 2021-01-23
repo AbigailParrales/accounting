e61448ed05d0cf01800e5a5dcd6eb1989b0e2379
AbigailParrales <danielaparralesd.m@gmail.com>
2021-01-23 15:48:53 -0600

    Move functions to tools file and rename variables

--- TicketControl/window.py
+++ TicketControl/window.py
@@ -1,67 +1,39 @@
 import PySimpleGUI as sg
 
 from Nota import Nota
-
-BACKGROUND = 'light yellow'
+import tools
 
 nota = Nota()
 
-def genColumnObjs(col, width):
-    newList=[]
-    for row in col:
-        newList.append(sg.Text(row, size=(width, 1)))
-    
-    return newList
-
-def makeNota(objNota):
-    # ["Cantidad", "Descripcion", "Precio Unitario", "Importe"]
-    # Have 4 colums
-    cantidad = sg.Column([objNota.getCantidades()])
-    descripcion = sg.Column([objNota.getDescripciones()])
-    pUnit = sg.Column([objNota.getPUnitarios()])
-    importe = sg.Column([objNota.getImportes()])
-
-    return [cantidad, descripcion, pUnit, importe]
-
-
-
 # Main window
-title_region = [
-    sg.Text("Nota de venta", text_color='forest green', 
-     font=('Helvetica',36, 'bold'), background_color=BACKGROUND)
+topRegion = [
+    sg.Text("Capturar venta", text_color='forest green', 
+     font=('Helvetica',36, 'bold'), background_color=tools.BACKGROUND)
  ]
-bottom_region = [
-    sg.Button('Imprimir', key='print', button_color=('black', 'forest green'))
+bottomRegion = [
+    sg.Button('Vista previa de nota', key='genNota', button_color=('black', 'forest green'))
  ]
-left_region = [
+midRegion = [
     [sg.Text("Cantidad", size=(15, 1)), sg.Input(key='cantidad', do_not_clear = False)],
     [sg.Text("Descripcion", size=(15, 1)), sg.Input(key='descripcion', do_not_clear = False)],
     [sg.Text("Precio unitario", size=(15, 1)), sg.Input(key='pUnit', do_not_clear = False)],
     [sg.Button('Registrar compra', key='compra', button_color=('black', 'forest green'))]
  ]
-right_region = [
-    [
-        # sg.Column([[sg.Text("Cantidad")], [sg.Text("Cantidad2")]]),
-        sg.Column([[sg.Text(x)] for x in nota.cantidades]),
-        sg.Column([[sg.Text(x)] for x in nota.descripciones]),
-        sg.Column([[sg.Text(x)] for x in nota.pUnitarios]),
-        sg.Column([[sg.Text(x)] for x in nota.importes])
-     ]
- ]
+
 layout = [
-    title_region,
-    [
-        sg.Column(left_region),
-        sg.VerticalSeparator(),
-        sg.Column(right_region,key="cantidades", scrollable= True, size=(400,400))
-        # sg.Column(genColumnObjs(nota.cantidades, 14),key="cDer", scrollable= True, size=(400,400))
-     ],
-    bottom_region
+    topRegion,
+    [sg.Column(midRegion)],
+    # [
+    #     sg.Column(left_region),
+    #     sg.VerticalSeparator(),
+    #     sg.Column(right_region,key="table", scrollable= True, size=(400,400))
+    #  ],
+    bottomRegion
  ]
 
 
 # Create the windows
-main_window = sg.Window('Nota de venta', layout, background_color=BACKGROUND)
+main_window = sg.Window('Generador de notas', layout, background_color=tools.BACKGROUND)
 
 # Display and interact with the Window using an Event Loop
 while True:
@@ -71,17 +43,13 @@ while True:
         print(f"Cantidad: {values['cantidad']}\tDescripcion: {values['descripcion']}\tPrecio: {values['pUnit']}")
         nota.registrarVenta(int(values['cantidad']), values['descripcion'], int(values['pUnit']))
 
-    elif event == 'print':
-        # print(type(nota))
-        # print(dir(nota))
-        right_region = nota.getCantidades()
-        main_window['cDer'].update(right_region)
-        # main_window['strNota'].update(nota.__str__())
+    elif event == 'genNota':
+        nota_window = tools.makeNota(nota)
+        event1, values1 = nota_window.read()
         print(nota)
 
     # See if window was closed
     elif event == sg.WINDOW_CLOSED:
-        # print(nota)
         break
 
 # Finish up by removing from the screen
