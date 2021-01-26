62ce25227daf328d25e8c668796c5b79c2132c9a
AbigailParrales <danielaparralesd.m@gmail.com>
2021-01-26 12:05:07 -0600

    Adding typography

--- TicketControl/tools.py
+++ TicketControl/tools.py
@@ -1,6 +1,14 @@
 import PySimpleGUI as sg
 
 BACKGROUND = 'light yellow'
+TITLE_COLOR_0 = 'forest green'
+TEXT_COLOR_0 = 'burlywood4'
+
+BUTTON_GOLD = ['black', 'gold']
+
+
+TITLE_FONT_0 = ('Helvetica',36, 'bold')
+TEXT_FONT_0 = ('Helvetica',24, 'bold')
 
 def genColumnObjs(col, width):
     newList=[]
